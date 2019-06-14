73eb2a27641b728e1fbc8110634137dd4c7833c6
Roland Studer <roland.studer@gmail.com>
2019-06-14 13:54:49 +0200

    pulling data from hack open data

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 New static page
 
-## Fragen:
+Pulls the session-name / descriptions from https://hack.opendata.ch/api/event/19/projects.json
 
-* Woher bekomme ich die Schrift: M3MonoRegular
+Enter dribdatIds in `timeTable.yml` like that:
+
+    -
+      time: 10:30 - 11:15
+      title: Parallele Unconference-Sessions (4-5 Gruppen)
+      type: sessions
+      dribdatIds:
+        - 365
+        - 366
+        - 367
+        - 368
--- assets/javascript/session_grid.js
+++ assets/javascript/session_grid.js
@@ -1,32 +1,67 @@
-new Vue({
-  el: "#session-grid",
-  data: {
-   info: undefined,
-   error: undefined,
-   timeTable: null,
-   rooms: [
-     {column: 'mainroom', name: 'Auditorium'},
-     {column: 'room1', name: 'Raum 1'},
-     {column: 'room2', name: 'Raum 2'},
-     {column: 'room3', name: 'Raum 3'},
-   ],
-   openSlotLabel: "Offener Slot für deine Session",
-  },
-  methods: {
-    getData: function() {
-      var options = {
-        callback: 'fn',
-        alt: 'json-in-script'
-      };
-      this.$http.jsonp('https://spreadsheets.google.com/feeds/list/15WkiIDfh8jYDHtr4gZviDfcgr1mHnbfBqKJyo8Ia82Y/1/public/values?alt=json-in-script', options).then(function(data) {
-        this.info = ''
-        this.timeTable = data
-      }, function(error) {
-        this.info = "Fehler beim Laden…";
-      });
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open('GET', 'https://hack.opendata.ch/api/event/current/challenges.json', true);
+xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == 4) {
+    if(xmlhttp.status == 200) {
+      var obj = JSON.parse(xmlhttp.responseText);
+      var sessions = obj.challenges;
+      sessions.forEach(function(session) {
+        updateSessionCell(session);
+      })
     }
   }
-}).getData();
+};
+xmlhttp.send(null);
+
+function updateSessionCell(session) {
+  elem('programm-session-title').innerHTML = session.name;
+  elem('programm-session-description').innerHTML = session.summary;
+  elem('programm-session-link').setAttribute('href', url());
+
+  function elem(klass) {
+    var selector = "#session-" + session.id + ' .' + klass
+    return document.querySelectorAll(selector)[0]
+  }
+
+  function url() {
+    return "https://hack.opendata.ch/project/" + session.id
+  }
+}
+
+
+// new Vue({
+//   el: "#session-grid",
+//   data: {
+//    info: undefined,
+//    error: undefined,
+//    timeTable: null,
+//    rooms: [
+//      {column: 'mainroom', name: 'Auditorium'},
+//      {column: 'room1', name: 'Raum 1'},
+//      {column: 'room2', name: 'Raum 2'},
+//      {column: 'room3', name: 'Raum 3'},
+//    ],
+//    openSlotLabel: "Offener Slot für deine Session",
+//   },
+//   methods: {
+//     getData: function() {
+//       var options = {
+//         callback: 'fn',
+//         alt: 'json-in-script'
+//       };
+//       this.$http.jsonp('https://spreadsheets.google.com/feeds/list/15WkiIDfh8jYDHtr4gZviDfcgr1mHnbfBqKJyo8Ia82Y/1/public/values?alt=json-in-script', options).then(function(data) {
+//         this.info = ''
+//         this.timeTable = data
+//       }, function(error) {
+//         this.info = "Fehler beim Laden…";
+//       });
+//     }
+//   }
+// }).getData();
+
+
+
+
 
 
 // 2PACX-1vSTvqvkV_arDWgfvJWj87Iab1bfBryu6BO_t8i3d-wctO4NhBeSPo7d-1bDW3PJYvTEWsNMLL5WEIj3
